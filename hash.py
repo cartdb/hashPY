a38f485865c18442780352d65d4bab1f340fe4b0
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-09-27 20:10:44 -0500

    Update hash.py

--- hash.py
+++ hash.py
@@ -2,9 +2,6 @@ import hashlib
 import zlib
 import os
 file = input("Input File: ")
-filetxt = file.replace(".", "")
-filetxt = filetxt + ".txt"
-filetxt = open(filetxt, "w")
 if os.path.isfile(file):
     print("")
 else:
@@ -12,19 +9,11 @@ else:
 file = open(file, "rb")
 file = file.read()
 print(hex(zlib.adler32(file)).replace("0x", ""))
-filetxt.write(hex(zlib.adler32(file)).replace("0x", ""))
 print(hex(zlib.crc32(file)).replace("0x", ""))
-filetxt.write("\n" + hex(zlib.crc32(file)).replace("0x", ""))
 print(hashlib.md5(file).hexdigest())
-filetxt.write("\n" + hashlib.md5(file).hexdigest())
 print(hashlib.sha1(file).hexdigest())
-filetxt.write("\n" + hashlib.sha1(file).hexdigest())
 print(hashlib.sha224(file).hexdigest())
-filetxt.write("\n" + hashlib.sha224(file).hexdigest())
 print(hashlib.sha256(file).hexdigest())
-filetxt.write("\n" + hashlib.sha256(file).hexdigest())
 print(hashlib.sha384(file).hexdigest())
-filetxt.write("\n" + hashlib.sha384(file).hexdigest())
 print(hashlib.sha512(file).hexdigest())
-filetxt.write("\n" + hashlib.sha512(file).hexdigest())
 filetxt.close()
